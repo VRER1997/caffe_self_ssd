997d862f11a2e6efbf4fc6dc743210cdc9c3ca22
1216778435@qq.com <1216778435@qq.com>
2020-01-28 21:51:26 +0800

    added data_reader

---
+++ include/caffe/data_reader.hpp
@@ -0,0 +1,14 @@
+#ifndef CAFFE_DATA_READER_HPP
+#define CAFFE_DATA_READER_HPP
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "caffe/common.hpp"
+#include "caffe/internal_thread.hpp"
+#include "caffe/util/blocking_queue.hpp"
+#include "caffe/util/db.hpp"
+
+#endif CAFFE_DATA_READER_HPP
+
---
+++ src/caffe/data_reader.cpp
@@ -0,0 +1,7 @@
+#include <boost/thread.hpp>
+#include <map>
+#include <string>
+#include <vector>
+
+#include "caffe/common.hpp"
+#include "caffe/data_reader.hpp"
